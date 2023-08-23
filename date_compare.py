fafd3bba763d8c1ea4ccdeb3afe200c6f45bfdb9
Pan <yp90240@win.edag.de>
2023-08-23 17:38:51 +0200

    Finish 1. version batch file to delete merged local / remote branches and empty trash bin

--- date_compare.py
+++ date_compare.py
@@ -15,5 +15,6 @@ def main():
     else:
         sys.exit(0)
 
+
 if __name__ == "__main__":
     main()
--- delete_merged_branch.bat
+++ delete_merged_branch.bat
@@ -1,30 +1,41 @@
 @echo off
+setlocal enabledelayedexpansion
 
 REM Fetch the latest data from the remote
 git fetch -p
 
-REM Delete local branches that have been merged into main
+REM REM Delete local branches that have been merged into main
 for /f "tokens=*" %%i in ('git branch --merged ^| findstr /v /c:"*" ^| findstr /v /c:"main"') do (
-    git branch -d %%i
+   git branch -d %%i
 )
 
 REM For each remote branch that's been merged into main, check its last commit date
 for /f "tokens=*" %%i in ('git branch -r --merged origin/main ^| findstr /v /c:"main" ^| findstr /c:"origin/"') do (
+    echo Processing branch: %%i
+
     REM Get the commit date for the branch
-    for /f "delims=" %%d in ('git show -s --format="%%ci" %%i') do set branchDate=%%d
+    Set "cmd=git show -s --format=%%ci refs/remotes/%%i"
+    
+    for /f "delims=" %%d in ('!cmd!') do set branchDate=%%d
+    
+    echo Last commit date for branch %%i: !branchDate!
 
     REM Use the Python script to check if the branchDate is more than 3 months old
-    python date_compare.py "%branchDate%"
+    python date_compare.py "!branchDate:~0,19!"
 
     REM If the python script returns 1 (indicating the branch is old), delete it.
-    if %errorlevel%==1 (
-        echo Deleting remote branch: %%i
-        REM Delete the branch remotely. Uncomment the line below to actually perform the deletion.
-        REM git push origin --delete %%i
+    if !errorlevel!==1 (
+        echo Deleting branch: %%i
+        REM Delete the branch both locally and remotely. Uncomment the line below to actually perform the deletion.
+
+        REM Strip the 'origin/' prefix to get the actual branch name
+        set branchName=%%i
+        set branchName=!branchName:origin/=!
+        git push origin --delete !branchName!
     )
 )
 
-REM Empty the Recycle Bin
+REM REM Empty the Recycle Bin
 echo Emptying the Recycle Bin...
 powershell -command "Clear-RecycleBin -Confirm:$false"
 echo Recycle Bin emptied.
